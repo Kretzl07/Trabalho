f8be09c8146136bdce3fe86748dac588d1cebd4d
LadyBug <105938774+Kretzl07@users.noreply.github.com>
2024-08-23 12:24:44 -0300

    ...

--- scrip.js
+++ scrip.js
@@ -9,12 +9,12 @@ const perguntas = [
         enunciado: "No âmbito social, você prefere:",
         alternativas: [
             {
-                texto: "Salvar todos os cachorros do mundo?",
-                afirmacao: "mas, em troca, todos os gatos morrem?",
+                texto: "Salvar todos os cachorros do mundo, mas, em troca, todos os gatos morrem?",
+                afirmacao: "Parabéns você acabou com o ecossistema",
             },
             {
-                texto: "Achar a cura do câncer?",
-                afirmacao: "mas, em troca, todas a sua familia morre?",
+                texto: "Achar a cura do câncer mas, em troca, todas a sua familia morre?",
+                afirmacao: "Parabéns perder sua família seria um sacrifício pessoal insuportável, e a dor emocional seria profunda e duradoura.",
             },
         ]
     },
@@ -22,12 +22,12 @@ const perguntas = [
         enunciado: "No âmbito ambiental, você prefere:",
         alternativas: [
             {
-                texto: "Não ocorrer mais enchentes?",
-                afirmacao: "mas, em troca, você não pode mais ir para a praia?",
+                texto: "Não ocorrer mais enchentes mas, em troca, você não pode mais ir para a praia",
+                afirmacao: "Parabéns a prevenção de enchentes evitaria danos econômicos e perda de vidas, além de proteger ecossistemas e propriedades.",
             },
             {
-                texto: "Acabar com o desmatamento do Mundo?",
-                afirmacao:"mas, troca, o Leonardo de Caprio nunca mais faz filme",
+                texto: "Acabar com o desmatamento do Mundo mas, troca, o Leonardo de Caprio nunca mais faz filme",
+                afirmacao:"Parabéns a eliminação do desmatamento teria um impacto ambiental extremamente positivo, preservando habitats naturais, combatendo as mudanças climáticas e protegendo espécies ameaçadas.",
             },
         ]
     },
@@ -35,12 +35,12 @@ const perguntas = [
         enunciado: "No âmbito tecnológico, você prefere:",
         alternativas: [
             {
-                texto: "Ter o celular mais top do Mundo mas você não tem internet fora de casa?",
-                afirmacao: "mas, em troca, você teria que aceitar a implementação de uma vigilância constante em todos os dispositivos conectados à internet?",
+                texto: "Ter o celular mais top do Mundo mas você não tem internet fora de casa mas, em troca, você teria que aceitar a implementação de uma vigilância constante em todos os dispositivos conectados à internet",
+                afirmacao: "Parabéns a falta de internet fora de casa limitaria severamente a utilidade do celular, tornando-o quase inútil em muitos cenários do dia a dia, como navegação, comunicação e trabalho remoto.",
             },
             {
-                texto: "Dar um celular para todos as pessoas?",
-                afirmacao: "mas, em troca, você nunca mais terá acesso à tecnologia?",
+                texto: "Dar um celular para todos as pessoas mas, em troca, você nunca mais terá acesso à tecnologia",
+                afirmacao: " Parabéns não ter mais acesso à tecnologia significaria ficar sem todas as ferramentas digitais que você usa no cotidiano, desde comunicação e entretenimento até ferramentas de trabalho e educação. Isso poderia ser um grande sacrifício pessoal em troca do benefício global.",
             },
         ]
     },
